465d3bcf8bf21f35dba16cf789ab63a7cb907c3c
Tejal-gole <https://github.com/Tejal-gole>
2022-01-21 23:00:57 +0530

    python assignment 1 and 2

---
+++ python assignment/python_assignment1.py
@@ -0,0 +1,16 @@
+import math
+
+#question1
+ans=[ i  for i in range(2000, 3201) if i %7 ==0 and i % 5 !=0]
+print(ans)
+
+#question2
+name=input("Enter your first name and last name: ")
+print("reverse order",name[::-1])
+
+#question3
+dimeter=12
+vol_of_sphere=1*math.pi*(dimeter/2)**3
+print("volume of sphere =",vol_of_sphere)
+
+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
